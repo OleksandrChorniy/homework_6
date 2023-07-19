54132eb3a354124483f07cfb7cab9b949b291344
OleksandrChorniy <sahacherniy94@gmail.com>
2023-07-19 17:23:30 +0300

    Add `run()` function

--- file_parser.py
+++ file_parser.py
@@ -16,31 +16,40 @@ MY_OTHER = []
 ARCHIVES = []
 
 REGISTER_EXTENSION = {
-    'JPEG': JPEG_IMG,
-    'JPG': JPG_IMG,
-    'PNG': PNG_IMG,
-    'SVG': SVG_IMG,
-    'MP3': MP3_AUDIO,
-    'TXT': TXT_DOCUMENTS,
-    'XLSX': XLSX_DOCUMENTS,
-    'DOC': DOC_DOCUMENTS,
-    'AVI': AVI_VIDEO,
-    'MOV': MOV_VIDEO,
-    'MP4': MP4_VIDEO,
-    'ZIP': ARCHIVES  
+    "JPEG": JPEG_IMG,
+    "JPG": JPG_IMG,
+    "PNG": PNG_IMG,
+    "SVG": SVG_IMG,
+    "MP3": MP3_AUDIO,
+    "TXT": TXT_DOCUMENTS,
+    "XLSX": XLSX_DOCUMENTS,
+    "DOC": DOC_DOCUMENTS,
+    "AVI": AVI_VIDEO,
+    "MOV": MOV_VIDEO,
+    "MP4": MP4_VIDEO,
+    "ZIP": ARCHIVES,
 }
 
 FOLDERS = []
-EXTENSION = set() 
+EXTENSION = set()
 UNKNOWN = set()
+
 
 def get_extension(filename: str) -> str:
     return Path(filename).suffix[1:].upper()
 
-def scan(folder: Path) -> None: 
+
+def scan(folder: Path) -> None:
     for item in folder.iterdir():
         if item.is_dir():
-            if item.name not in ('archives', 'video', 'audio', 'documents', 'images', 'MY_OTHER'):
+            if item.name not in (
+                "archives",
+                "video",
+                "audio",
+                "documents",
+                "images",
+                "MY_OTHER",
+            ):
                 FOLDERS.append(item)
                 scan(item)
             continue
@@ -58,6 +67,11 @@ def scan(folder: Path) -> None:
                 UNKNOWN.add(ext)
                 MY_OTHER.append(fullname)
 
-if __name__ == "__main__":
+
+def run():
     folder_to_scan = sys.argv[1]
     scan(Path(folder_to_scan))
+
+
+if __name__ == "__main__":
+    run()
--- main.py
+++ main.py
@@ -4,28 +4,35 @@ import sys
 import file_parser as parser
 from normalize import normalize
 
+
 def handle_media(filename: Path, target_folder: Path) -> None:
     target_folder.mkdir(exist_ok=True, parents=True)
     filename.replace(target_folder / normalize(filename.name))
+
 
 def handle_document(filename: Path, target_folder: Path) -> None:
     target_folder.mkdir(exist_ok=True, parents=True)
     filename.replace(target_folder / normalize(filename.name))
 
+
 def handle_other(filename: Path, target_folder: Path) -> None:
     target_folder.mkdir(exist_ok=True, parents=True)
     filename.replace(target_folder / normalize(filename.name))
 
+
 def handle_archive(filename: Path, target_folder: Path) -> None:
     target_folder.mkdir(exist_ok=True, parents=True)
-    folder_for_file = target_folder / normalize(filename.name.replace(filename.suffix, ''))
+    folder_for_file = target_folder / normalize(
+        filename.name.replace(filename.suffix, "")
+    )
     folder_for_file.mkdir(exist_ok=True, parents=True)
     try:
         shutil.unpack_archive(filename, folder_for_file)
     except shutil.ReadError:
-        print('NOT archive')
+        print("NOT archive")
         folder_for_file.rmdir()
     filename.unlink()
+
 
 def handel_folder(folder: Path):
     try:
@@ -33,41 +40,47 @@ def handel_folder(folder: Path):
     except OSError:
         print(f"Can't delete {folder}")
 
+
 def main(folder: Path):
     parser.scan(folder)
     for file in parser.JPEG_IMG:
-        handle_media(file, folder / 'images' / 'JPEG')
+        handle_media(file, folder / "images" / "JPEG")
     for file in parser.JPG_IMG:
-        handle_media(file, folder / 'images' / 'JPG')
+        handle_media(file, folder / "images" / "JPG")
     for file in parser.PNG_IMG:
-        handle_media(file, folder / 'images' / 'PNG')
+        handle_media(file, folder / "images" / "PNG")
     for file in parser.SVG_IMG:
-        handle_media(file, folder / 'images' / 'SVG')
+        handle_media(file, folder / "images" / "SVG")
     for file in parser.MP3_AUDIO:
-        handle_media(file, folder / 'audio' / 'MP3')
+        handle_media(file, folder / "audio" / "MP3")
     for file in parser.AVI_VIDEO:
-        handle_media(file, folder / 'video' / 'AVI')
+        handle_media(file, folder / "video" / "AVI")
     for file in parser.MOV_VIDEO:
-        handle_media(file, folder / 'video' / 'MOV')
+        handle_media(file, folder / "video" / "MOV")
     for file in parser.MP4_VIDEO:
-        handle_media(file, folder / 'video' / 'MP4')
+        handle_media(file, folder / "video" / "MP4")
 
     for file in parser.TXT_DOCUMENTS:
-        handle_document(file, folder / 'documents' / 'TXT')
+        handle_document(file, folder / "documents" / "TXT")
     for file in parser.XLSX_DOCUMENTS:
-        handle_document(file, folder / 'documents' / 'XLSX')
+        handle_document(file, folder / "documents" / "XLSX")
     for file in parser.DOC_DOCUMENTS:
-        handle_document(file, folder / 'documents' / 'DOC')
+        handle_document(file, folder / "documents" / "DOC")
 
     for file in parser.MY_OTHER:
-        handle_other(file, folder / 'MY_OTHER')
+        handle_other(file, folder / "MY_OTHER")
     for file in parser.ARCHIVES:
-        handle_archive(file, folder / 'ARCHIVES')
+        handle_archive(file, folder / "ARCHIVES")
 
     for folder in parser.FOLDERS[::-1]:
         handel_folder(folder)
 
-if __name__ == "__main__":
+
+def run():
     if sys.argv[1]:
         folder_for_scan = Path(sys.argv[1])
         main(folder_for_scan.resolve())
+
+
+if __name__ == "__main__":
+    run()
